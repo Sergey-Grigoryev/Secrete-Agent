5e25b1037197eed7754d25e6dd99cc379e4c9d50
Sergey-Grigoryev <ruserge88@gmail.com>
2025-04-19 10:58:30 -0700

    Porting over work from DA Repo

--- src/gadgets/decryptor.py
+++
@@ -1,9 +0,0 @@
-def decrypt_message(ciphertext, shift):
-    decrypted = []
-    for char in ciphertext:
-        if char.isalpha():
-            shift_char = chr(((ord(char) - 65 - shift) % 26) + 65)
-            decrypted.append(shift_char)
-        else:
-            decrypted.append(char)
-    return f"Unencrypted message: {''.join(decrypted)}"
